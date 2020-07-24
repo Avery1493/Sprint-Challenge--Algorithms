ec4a7785d81f765eb1a431cfd00d6b88363534a3
Avery Quinn <avery.quinn1493@gmail.com>
2020-07-24 15:07:12 -0700

    ..

--- Short-Answer/Algorithms_Answers.md
+++ Short-Answer/Algorithms_Answers.md
@@ -2,13 +2,13 @@
 
 ## Exercise I
 
-a) O(n) The runtime grows at the same rate for each input. a become n^3.
+a) O(n) The runtime grows at the same rate for each input. a becomes n^3.
 
 
 b) O(n log n) The algorithm has to loop through each input, but the runtime is slightly decreased each time because j increases by two. 
 
 
-c) O(n) For each bunny, 3 is added.
+c) O(n) For each bunny, 2 is added.
 
 ## Exercise II
 Since floors of a building are typically in order, you use a binary seach to guess which floor is f. Binary search will always start in the middle and eliminate half of the floors. The runtime for would be O(log n)
--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -97,45 +97,43 @@ class SortingRobot:
         Sort the robot's list.
         """
         # if can't move right and light is off
-            # return self
         if self.can_move_right() == False and self.light_is_on() == False:
             return self
-
-        # turn light on
+        
+        self.set_light_on() # turn light on
+        print("Light ON")
         # while light is on 
-        self.set_light_on()
         while self.light_is_on() == True:
             # pick up item -- swap
-            self.swap_item()
-            self.set_light_off()
+            if self.can_move_right() == True:
+                self.swap_item()
+            self.set_light_off()    # turn light off
             # while can move right
             while self.can_move_right() == True:
                 # move right
                 self.move_right()
-
-                # compare position with item -- compare
                 # if item > position -- == 1
                 if self.compare_item() == 1:
                     # swap
                     self.swap_item()
                     # turn light on
                     self.set_light_on()
-                
-            # while can move left
-            while self.can_move_left() == True:
+            
+            # if self.can_move_right() == False and self.compare_item() == None:
+            #     self.set_light_off()
+
+            while self.can_move_left() == True and self.compare_item() != None:
                 # move left
                 self.move_left()
 
-                # compare position with item -- compare
-                # if compare == None
-                if self.compare_item() == None:
-                    # drop item swap
-                    self.swap_item()
-        
+                self.move_left()
+                self.swap_item()
+                print("item", self._item)
+                self.move_right()
+
+            
+   
         return self
-
-
-
 
 
 
@@ -144,7 +142,7 @@ if __name__ == "__main__":
     # with `python robot_sort.py`
 
     #l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1, 45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
-    l = [15, 10, 8, 20, 6, 3]
+    l = [10, 3, 6, 8, 16, 11]
     robot = SortingRobot(l)
 
     print(robot.sort())
